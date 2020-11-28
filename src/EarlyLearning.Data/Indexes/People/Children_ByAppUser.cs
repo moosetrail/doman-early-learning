72dcb51b8cb33a351f7f401b00f01f0d6baa885b
Johanna Hultén <min.mail@johannap.se>
2020-11-28 21:00:35 +0100

    Updated Children_ByAppUser to use result class and return children

--- src/EarlyLearning.Data/Indexes/People/Children_ByAppUser.cs
+++ src/EarlyLearning.Data/Indexes/People/Children_ByAppUser.cs
@@ -1,14 +1,27 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using EarlyLearning.Dataclasses.People;
 using Raven.Client.Documents.Indexes;
 
 namespace EarlyLearning.Data.Indexes.People
 {
-    public class Children_ByAppUser : AbstractMultiMapIndexCreationTask
+    public class Children_ByAppUser : AbstractMultiMapIndexCreationTask<Children_ByAppUser.Result>
     {
+        public class Result
+        {
+            public string Email { get; set; }
+
+            public IEnumerable<Child> Children { get; set; }
+        }
+
         public Children_ByAppUser()
         {
-            AddMap<AdultProgrammer>(adults => from a in adults select new { a.Email, a.ChildIds });
+            AddMap<AdultProgrammer>(adults => from a in adults
+                select new Result
+                {
+                    Email = a.Email,
+                    Children = LoadDocument<Child>(a.ChildIds)
+                });
         }
     }
 }
