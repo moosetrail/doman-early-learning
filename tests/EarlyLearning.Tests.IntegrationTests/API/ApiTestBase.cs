c0b593504238f22cc941b420b577d22ff315a325
Johanna Hultén <min.mail@johannap.se>
2020-11-28 20:28:22 +0100

    Added ApiTestBase class for integration tests

---
+++ tests/EarlyLearning.Tests.IntegrationTests/API/ApiTestBase.cs
@@ -0,0 +1,53 @@
+﻿using System.Net.Http;
+using System.Text;
+using EarlyLearning.API;
+using EarlyLearning.Tests.TestHelpers.TestFactory;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using NUnit.Framework;
+
+namespace EarlyLearning.Tests.IntegrationTests.API
+{
+    public abstract class ApiTestBase
+    {
+        protected static readonly TestFactory TestFactory = new TestFactory();
+
+        protected static readonly WebApplicationFactory<Startup> ServerFactory = new EarlyLearningAPIFactory<Startup>(TestFactory);
+
+        protected HttpClient SUT;
+
+        [SetUp]
+        protected void Setup()
+        {
+            SUT = ServerFactory.WithWebHostBuilder(config =>
+            {
+                config.UseSetting("RavenDbLocation", "http://localhost:5100/");
+                config.UseSetting("CORS", "http://localhost");
+            }).CreateClient();
+        }
+
+        [TearDown]
+        protected void Teardown()
+        {
+            SUT.Dispose();
+        }
+
+        protected StringContent ToJson(object obj)
+        {
+            var json = JsonConvert.SerializeObject(obj, Formatting.Indented, new JsonSerializerSettings
+            {
+                ContractResolver = new CamelCasePropertyNamesContractResolver(),
+                DateTimeZoneHandling = DateTimeZoneHandling.Unspecified
+            });
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+            return content;
+        }
+
+        protected string ToJsonString(object obj)
+        {
+            var json = ToJson(obj);
+            return json.ReadAsStringAsync().Result;
+        }
+    }
+}
