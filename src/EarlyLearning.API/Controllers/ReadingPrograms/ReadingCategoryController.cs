d259b55e785d6e865db2bca55d48aa68bc7e013b
Johanna Hultén <min.mail@johannap.se>
2021-02-02 18:10:38 +0100

    Mapped out the methods needed for the first stage of development

--- src/EarlyLearning.API/Controllers/ReadingPrograms/ReadingCategoryController.cs
+++ src/EarlyLearning.API/Controllers/ReadingPrograms/ReadingCategoryController.cs
@@ -13,5 +13,43 @@ namespace EarlyLearning.API.Controllers.ReadingPrograms
         {
             return null;
         }
+
+        [HttpGet]
+        [Route("planned")]
+        public Task<IActionResult> GetPlanned([FromQuery] string programId, [FromQuery] int limit = 10,
+            [FromQuery] int offset = 0)
+        {
+            return null;
+        }
+
+        [HttpGet]
+        [Route("retired")]
+        public Task<IActionResult> GetRetired([FromQuery] string programId, [FromQuery] int limit = 10,
+            [FromQuery] int offset = 0)
+        {
+            return null;
+        }
+
+        [HttpPost]
+        [Route("")]
+        private Task<IActionResult> Add([FromBody] object unitToAdd, [FromQuery] string programId)
+        {
+            return null;
+        }
+
+        [HttpPatch]
+        [Route("status")]
+        public Task<IActionResult> ChangeStatus([FromQuery] string unitId, [FromQuery] int newStatus)
+        {
+            return null; 
+        }
+
+        [HttpPatch]
+        [Route("move")]
+        public Task<IActionResult> MovePlanned([FromQuery] string unitId, [FromQuery] string programId,
+            [FromQuery] int toSpot)
+        {
+            return null;
+        }
     }
 }
